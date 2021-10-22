631aaaebc8634fb7320dbb7c97573158da4aed14
kitUIN <kulujun@gmail.com>
2021-10-22 16:44:09 +0800

    导航栏样式

--- MainPage.cpp
+++ MainPage.cpp
@@ -1,12 +1,17 @@
 ﻿#include "pch.h"
 #include "MainPage.h"
+
 #include "MainPage.g.cpp"
 
+
+using namespace std;
 using namespace winrt;
 using namespace Windows::UI::Xaml;
 using namespace Windows::UI::Xaml::Hosting;
 using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Media::Imaging;
 using namespace Windows::ApplicationModel::Core;
+using namespace Windows::Foundation;
 using namespace Windows::UI::ViewManagement;
 
 namespace winrt::bikabika::implementation
@@ -14,7 +19,7 @@ namespace winrt::bikabika::implementation
     MainPage::MainPage()
     {
         InitializeComponent();
-        
+       
     }
 
     int32_t MainPage::MyProperty()
@@ -26,15 +31,108 @@ namespace winrt::bikabika::implementation
     {
         throw hresult_not_implemented();
     }
+    
 
-    Windows::UI::Composition::ContainerVisual MainPage::GetVisual(Windows::UI::Xaml::UIElement element)
+    // List of ValueTuple holding the Navigation Tag and the relative Navigation Page
+    std::vector<std::pair<std::wstring, Windows::UI::Xaml::Interop::TypeName>> m_pages;
+
+    void MainPage::NavView_Loaded(
+        Windows::Foundation::IInspectable const& /* sender */,
+        Windows::UI::Xaml::RoutedEventArgs const& /* args */)
     {
-        // 获取Xaml内的元素
-        auto hostVisual = ElementCompositionPreview::GetElementVisual(element);
-        auto root = hostVisual.Compositor().CreateContainerVisual();
-        ElementCompositionPreview::SetElementChildVisual(element, root);
-        return root;
+        
+        NavView().MenuItems().Append(muxc::NavigationViewItemSeparator());
+        muxc::NavigationViewItem navigationViewItem;
+        navigationViewItem.Content(winrt::box_value(L"My content"));
+        navigationViewItem.Icon(wuxc::SymbolIcon(static_cast<wuxc::Symbol>(0xF1AD)));
+        navigationViewItem.Tag(winrt::box_value(L"content"));
+
+        NavView().MenuItems().Append(navigationViewItem);
+        
+
+        NavView().SelectedItem(NavView().MenuItems().GetAt(0));
+        NavView_Navigate(L"home",
+            Windows::UI::Xaml::Media::Animation::EntranceNavigationTransitionInfo());
+
     }
+
+    void MainPage::NavView_ItemInvoked(
+        Windows::Foundation::IInspectable const& /* sender */,
+        muxc::NavigationViewItemInvokedEventArgs const& args)
+    {
+        if (args.IsSettingsInvoked())
+        {
+            NavView_Navigate(L"settings", args.RecommendedNavigationTransitionInfo());
+        }
+        else if (args.InvokedItemContainer())
+        {
+            NavView_Navigate(
+                winrt::unbox_value_or<winrt::hstring>(
+                    args.InvokedItemContainer().Tag(), L"").c_str(),
+                args.RecommendedNavigationTransitionInfo());
+        }
+    }
+
+    // NavView_SelectionChanged is not used in this example, but is shown for completeness.
+    // You will typically handle either ItemInvoked or SelectionChanged to perform navigation,
+    // but not both.
+    void MainPage::NavView_SelectionChanged(
+        muxc::NavigationView const& /* sender */,
+        muxc::NavigationViewSelectionChangedEventArgs const& args)
+    {
+        if (args.IsSettingsSelected())
+        {
+            NavView_Navigate(L"settings", args.RecommendedNavigationTransitionInfo());
+        }
+        else if (args.SelectedItemContainer())
+        {
+            NavView_Navigate(
+                winrt::unbox_value_or<winrt::hstring>(
+                    args.SelectedItemContainer().Tag(), L"").c_str(),
+                args.RecommendedNavigationTransitionInfo());
+        }
+    }
+
+    void MainPage::NavView_Navigate(
+        std::wstring navItemTag,
+        Windows::UI::Xaml::Media::Animation::NavigationTransitionInfo const& transitionInfo)
+    {
+        Windows::UI::Xaml::Interop::TypeName pageTypeName;
+        if (navItemTag == L"settings")
+        {
+           
+        }
+        else
+        {
+            for (auto&& eachPage : m_pages)
+            {
+                if (eachPage.first == navItemTag)
+                {
+                    pageTypeName = eachPage.second;
+                    break;
+                }
+            }
+        }
+        // Get the page type before navigation so you can prevent duplicate
+        // entries in the backstack.
+        Windows::UI::Xaml::Interop::TypeName preNavPageType =
+            ContentFrame().CurrentSourcePageType();
+
+        // Navigate only if the selected page isn't currently loaded.
+        if (pageTypeName.Name != L"" && preNavPageType.Name != pageTypeName.Name)
+        {
+            ContentFrame().Navigate(pageTypeName, nullptr, transitionInfo);
+        }
+    }
+
+    
+
+
+
+
+    
+
+
 
  
     
@@ -44,6 +142,3 @@ namespace winrt::bikabika::implementation
 
 
 }
-
-
-
--- MainPage.h
+++ MainPage.h
@@ -1,6 +1,15 @@
 ﻿#pragma once
 
 #include "MainPage.g.h"
+namespace muxc
+{
+    using namespace winrt::Microsoft::UI::Xaml::Controls;
+};
+
+namespace wuxc
+{
+    using namespace winrt::Windows::UI::Xaml::Controls;
+};
 
 namespace winrt::bikabika::implementation
 {
@@ -9,9 +18,27 @@ namespace winrt::bikabika::implementation
         MainPage();
         int32_t MyProperty();
         void MyProperty(int32_t value);
+        void NavView_Loaded(
+            Windows::Foundation::IInspectable const& /* sender */,
+            Windows::UI::Xaml::RoutedEventArgs const& /* args */);
+        void NavView_ItemInvoked(
+            Windows::Foundation::IInspectable const& /* sender */,
+            muxc::NavigationViewItemInvokedEventArgs const& args);
+
+        // NavView_SelectionChanged is not used in this example, but is shown for completeness.
+        // You'll typically handle either ItemInvoked or SelectionChanged to perform navigation,
+        // but not both.
+        void NavView_SelectionChanged(
+            muxc::NavigationView const& /* sender */,
+            muxc::NavigationViewSelectionChangedEventArgs const& args);
+        void NavView_Navigate(
+            std::wstring navItemTag,
+            Windows::UI::Xaml::Media::Animation::NavigationTransitionInfo const& transitionInfo);
     private:
-        Windows::UI::Composition::ContainerVisual GetVisual(Windows::UI::Xaml::UIElement element);
+        // Vector of std::pair holding the Navigation Tag and the relative Navigation Page.
+        std::vector<std::pair<std::wstring, Windows::UI::Xaml::Interop::TypeName>> m_pages;
     };
+    
 
 }
 
